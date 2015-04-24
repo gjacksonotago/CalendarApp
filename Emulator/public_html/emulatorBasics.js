00343d2a8f5be43a86f5c956728d4989522e5f75
gjacksonotago <george.o.jackson@gmail.com>
2015-04-24 17:33:00 +1200

    1.4.4 - Scaled and Dynamic Canvas
    I scaled the canvas to the 320x320 px size specified in the assignment specs, and
    made the square clicking code and square drawing code both scale based on the size of the canvas element.
    This makes both much more dynamic and usable later in other ways.

--- Emulator/public_html/emulatorBasics.js
+++ Emulator/public_html/emulatorBasics.js
@@ -51,6 +51,9 @@
                 return stringDate;
             }
             
+            //Creates some data to emulate the watch!
+            //Need more functions maybe?
+            
             //Draws to the canvas - currently a red rectangle and
             // both the current date and time as retrieved by
             // the functions above.
@@ -64,13 +67,13 @@
 
                 //Changes colour of the Canvas Element
                 ctx.fillStyle = "#FF0000";
-                ctx.fillRect(0, 0, 500, 500);
+                ctx.fillRect(0, 0, 320, 320);
 
                 //Writes Time as a String to the Canvas
                 ctx.fillStyle = "#000000";
                 ctx.font = "48px Sans Serif";
                 ctx.fillText(stringTime, 10, 50);
-                ctx.fillText(stringDate, 10, 100);
+                ctx.fillText(stringDate, 10, 300);
                 
                 //creates the black rectangle in the middle
                 drawClickRect();
@@ -81,8 +84,10 @@
             function drawClickRect() {
                 var c = document.getElementById("canvas_1");
                 var ctx = c.getContext("2d");
+                var width = c.width;
+                var height = c.height;
                 
                 ctx.fillStyle = "#000000";
-                ctx.fillRect(125, 125, 250, 250);
+                ctx.fillRect(width/4, height/4, width/2, height/2);
             }
             
