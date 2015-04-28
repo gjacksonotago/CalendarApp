b6a7631201d384f2acccae52269a2b45b5d82dd6
gjacksonotago <george.o.jackson@gmail.com>
2015-04-28 21:28:21 +1200

    1.7.1 - Code Generalisation + Cleanup
    Fucked with the methods to be more general for use later when drawing
    other bits and pieces.
    
    Commented code, and am working on a working initialisation state that
    can be saved - currently, the emulateVibrate doesn't do anything
    useful....need to work on that.

--- Emulator/public_html/emulatorBasics.js
+++ Emulator/public_html/emulatorBasics.js
@@ -11,6 +11,16 @@
 
 //Global variables
 var canvasWidth, canvasHeight;
+var oldCanvas = '<canvas width="' + 320 + '" height="' + 320 
+           + '" id="' + 'canvas_1"' + 'style="' + 
+           'border:1px solid #000000;">' +
+            'Canvas Tag not Supported by your browser version!' +
+            '</canvas>';
+var canvasString = 
+            '<canvas width="' + 340 + '" height="' + 340 + 
+            '" id="' + 'canvas_1"' +
+            'style="' + 'border:1px solid #000000;"' + '>' +
+            '</canvas>';
 
 /*
  * These functions are used for the mouse cordinates
@@ -69,7 +79,7 @@ function emulatorIntialise() {
     //creates the "app" icon in the top left corner:
     ctx.fillStyle = "#000000";
     ctx.fillRect(10, 10, 50, 50);
-    mouseDown(10, 10, 50, 50);
+    mouseDownCoords(10, 10, 50, 50);
 }
 
 //Creates some data to emulate the watch!
@@ -101,18 +111,93 @@ function drawToCanvas() {
     //creates the black rectangle in the middle
     drawClickRect();
 }
-function mouseOver() {
+
+/*
+ * Creates a mouseover listener at the specified location
+ * (worked out using an (x, y) origin point and the width/height
+ * passed as variables) to execute 'actionTaken' on mouseover.
+ * 
+ * @param {int} xPosition
+ * @param {int} yPosition
+ * @param {int} xSize
+ * @param {int} ySize
+ * @param {function} actionTaken
+ * @returns {undefined}
+ */
+function mouseOver(xPosition, yPosition, xSize, ySize, actionTaken) {
+    var canvas = document.getElementById('canvas_1');
+
+    canvas.addEventListener('mouseover', function() {
+
+        var xCalc = (canvasWidth + xSize + xPosition) - canvasWidth;
+        var yCalc = (canvasHeight + ySize + yPosition) - canvasHeight;
+
+        //If Mouse Clicked on the Black Square, new message!
+        if (mousePos.x <= xCalc && mousePos.y <= yCalc
+                && mousePos.x >= (xPosition) && mousePos.y >= (yPosition)) {
+                actionTaken();
+
+        }
+    }, false);
+}
+
+/*
+ * Adds a mouseover listener to the specified area using
+ * the canvas and passed (x, y) and size variables to create
+ * the area.
+ * 
+ * This listener specifically only does writing co-ordinates 
+ * to the HTML.
+ * 
+ * @param {int} xPosition
+ * @param {int} yPosition
+ * @param {int} xSize
+ * @param {int} ySize
+ * @returns {undefined}
+ */
+function mouseOverCoords(xPosition, yPosition, xSize, ySize) {
     var canvas = document.getElementById('canvas_1');
 
     //Mouseover event for updating the time on the
     //canvas element!
-    canvas.addEventListener('mouseover', function() {
-        drawToCanvas();     
+    canvas.addEventListener('mouseover', function(evt) {
+        /*
+         * Grabs mouse position co-ords and creates a 
+         * string out of them to use later.
+         */
+        var mousePos = getMousePos(canvas, evt);
+        var message = 'Mouse position: ' + mousePos.x + ', ' + 
+                Math.round(mousePos.y);
+        var sqMes = 'SQUARE CLICKED: ' + mousePos.x + ', ' + 
+                Math.round(mousePos.y);
+        
+        var xCalc = (canvasWidth + xSize + xPosition) - canvasWidth;
+        var yCalc = (canvasHeight + ySize + yPosition) - canvasHeight;
+
+        //If Mouse Clicked on Square, new message!
+        if (mousePos.x <= xCalc && mousePos.y <= yCalc
+                && mousePos.x >= (xPosition) && mousePos.y >= (yPosition)) {
+                document.getElementById("divShow").innerHTML = sqMes;
+
+        } else { 
+            //writeMessage(canvas, message);
+            //Print the Mouse Co-ords to divShow.
+            document.getElementById("divShow").innerHTML = message;
+        }
     }, false);
 }
 
-//Adds a mouseDown function at the given position
-function mouseDown(xPosition, yPosition, xSize, ySize) {
+/*
+ * Adds a mousedown event listener that specifically deals with
+ * mouse co-ordinates on the canvas.
+ * 
+ * @param {int} xPosition
+ * @param {int} yPosition
+ * @param {int} xSize
+ * @param {int} ySize
+ * @returns {undefined}
+ */
+function mouseDownCoords(xPosition, yPosition, xSize, ySize) {
     var canvas = document.getElementById('canvas_1');
     
     //The mouseclick event listener.
@@ -145,21 +230,58 @@ function mouseDown(xPosition, yPosition, xSize, ySize) {
         }
     }, false);
 }
-//Calls any functions that initialise event listeners
-function listeners() {
-    mouseOver();
-    mouseDown();
+
+/*
+ * Adds a Mousedown event listener across the given
+ * co-ordinates - which it works out like the fillRectangle 
+ * method does, from a given (x, y) origin point and the size
+ * of the area to make interactive.
+ * 
+ * Utilises the canvas size properties to work the area out.
+ * 
+ * The event listener executes the function 'actionTaken'
+ * upon mousedown.
+ * 
+ * @param {int} xPosition
+ * @param {int} yPosition
+ * @param {int} xSize
+ * @param {int} ySize
+ * @param {function} actionTaken
+ * @returns {undefined}
+ */
+function mouseDown(xPosition, yPosition, xSize, ySize, actionTaken) {
+    var canvas = document.getElementById('canvas_1');
+    
+    //The mouseclick event listener.
+    canvas.addEventListener('mousedown', function(evt) {
+        
+        var xCalc = (canvasWidth + xSize + xPosition) - canvasWidth;
+        var yCalc = (canvasHeight + ySize + yPosition) - canvasHeight;
+
+        //If Mouse Clicked on the Black Square, new message!
+        if (mousePos.x <= xCalc && mousePos.y <= yCalc
+                && mousePos.x >= (xPosition) && mousePos.y >= (yPosition)) {
+                actionTaken();
+        } 
+    }, false);
 }
 
-//Creates a black rectangle on the canvas
-function drawClickRect() {
+//Calls any functions that initialise event listeners
+function listeners(xPos, yPos, xSize, ySize, actionToTake) {
+    mouseOver(xPos, yPos, xSize, ySize, actionToTake);
+    mouseDown(xPos, yPos, xSize, ySize, actionToTake);
+}
+
+//Creates a black rectangle on the canvas with a mouseDown listener
+// which listens for 'actionToTake'
+function drawClickRect(xPos, yPos, xSize, ySize, actionToTake) {
     var c = document.getElementById("canvas_1");
     var ctx = c.getContext("2d");
-    var width = c.width;
-    var height = c.height;
-
     ctx.fillStyle = "#000000";
-    ctx.fillRect(width/4, height/4, width/2, height/2);
+    //Create the rectangle
+    ctx.fillRect(xPos, yPos, xSize, ySize);
+    //Add the clickable listener
+    mouseDown(xPos, yPos, xSize, ySize, actionToTake);
 }
 
 //Will soon (HOPEFULLY) convert a date/time to a string
@@ -171,32 +293,24 @@ function dateStringForJSON(toConvert) {
     return toConvert;
 }
 
-//Should Replace the Content of CanvasDiv with a larger
-// Canvas object.
+/*
+ * This function enlarges the current canvas and 
+ * then shrinks it after a preset interval.
+ * 
+ * This emulates a "vibration" function.
+ */
 function emulateVibrate() {
-    var canvasString = 
-            '<canvas width="' + 340 + '" height="' + 340 + 
-            '" id="' + 'canvas_1"' +
-            'style="' + 'border:1px solid #000000;"' + '>' +
-            '</canvas>';
-
     document.getElementById("canvasDiv").innerHTML = canvasString;
     drawToCanvas();
-
     //set a short time interval
     setTimeout("reDrawOriginal()", 60);
 
 }
 
-//Redraw's what we had to begin with!
+/*
+ * Redraws the canvas at it's original size.
+ */
 function reDrawOriginal() {
-
-   var oldCanvas = '<canvas width="' + 320 + '" height="' + 320 
-           + '" id="' + 'canvas_1"' + 'style="' + 
-           'border:1px solid #000000;">' +
-            'Canvas Tag not Supported by your browser version!' +
-            '</canvas>';
-
     document.getElementById("canvasDiv").innerHTML = oldCanvas;
     drawToCanvas();
     listeners();
