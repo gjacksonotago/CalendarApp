ea36df7828bb9361f1f6d8bb03f3905f32bc57c6
gjacksonotago <george.o.jackson@gmail.com>
2015-04-29 10:45:55 +1200

    1.8.0 - Deprecated and Removed old functions
    Realised I could simplify some of the functions so I've done that: will maybe make overrides for "else" cases
    in mouse listeners.

--- Emulator/public_html/emulatorBasics.js
+++ Emulator/public_html/emulatorBasics.js
@@ -11,6 +11,7 @@
 
 //Global variables
 var canvasWidth, canvasHeight;
+var message, sqMes;
 var oldCanvas = '<canvas width="' + 320 + '" height="' + 320 
            + '" id="' + 'canvas_1"' + 'style="' + 
            'border:1px solid #000000;">' +
@@ -79,38 +80,13 @@ function emulatorIntialise() {
     //creates the "app" icon in the top left corner:
     ctx.fillStyle = "#000000";
     ctx.fillRect(10, 10, 50, 50);
-    mouseDownCoords(10, 10, 50, 50);
+    mouseDown(10, 10, 50, 50, printPosition);
 }
 
 //Creates some data to emulate the watch!
 //Need more functions maybe?
 
-//Draws to the canvas - currently a red rectangle and
-// both the current date and time as retrieved by
-// the functions above.
-function drawToCanvas() {
-    var stringTime = createTime();
-    var stringDate = currentDate();
 
-    //Create the Canvas stuff
-    var c = document.getElementById("canvas_1");
-    var ctx = c.getContext("2d");
-    canvasWidth = c.width;
-    canvasHeight = c.height;
-
-    //Changes colour of the Canvas Element
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-
-    //Writes Time as a String to the Canvas
-    ctx.fillStyle = "#000000";
-    ctx.font = "48px Sans Serif";
-    ctx.fillText(stringTime, 10, (canvasHeight/10)+20);
-    ctx.fillText(stringDate, 10, (canvasHeight-20));
-
-    //creates the black rectangle in the middle
-    drawClickRect();
-}
 
 /*
  * Creates a mouseover listener at the specified location
@@ -127,7 +103,13 @@ function drawToCanvas() {
 function mouseOver(xPosition, yPosition, xSize, ySize, actionTaken) {
     var canvas = document.getElementById('canvas_1');
 
-    canvas.addEventListener('mouseover', function() {
+    canvas.addEventListener('mouseover', function(evt) {
+        
+        var mousePos = getMousePos(canvas, evt);
+        message = 'Mouse position: ' + mousePos.x + ', ' + 
+                Math.round(mousePos.y);
+        sqMes = 'SQUARE CLICKED: ' + mousePos.x + ', ' + 
+                Math.round(mousePos.y);
 
         var xCalc = (canvasWidth + xSize + xPosition) - canvasWidth;
         var yCalc = (canvasHeight + ySize + yPosition) - canvasHeight;
@@ -137,96 +119,6 @@ function mouseOver(xPosition, yPosition, xSize, ySize, actionTaken) {
                 && mousePos.x >= (xPosition) && mousePos.y >= (yPosition)) {
                 actionTaken();
 
-        }
-    }, false);
-}
-
-/*
- * Adds a mouseover listener to the specified area using
- * the canvas and passed (x, y) and size variables to create
- * the area.
- * 
- * This listener specifically only does writing co-ordinates 
- * to the HTML.
- * 
- * @param {int} xPosition
- * @param {int} yPosition
- * @param {int} xSize
- * @param {int} ySize
- * @returns {undefined}
- */
-function mouseOverCoords(xPosition, yPosition, xSize, ySize) {
-    var canvas = document.getElementById('canvas_1');
-
-    //Mouseover event for updating the time on the
-    //canvas element!
-    canvas.addEventListener('mouseover', function(evt) {
-        /*
-         * Grabs mouse position co-ords and creates a 
-         * string out of them to use later.
-         */
-        var mousePos = getMousePos(canvas, evt);
-        var message = 'Mouse position: ' + mousePos.x + ', ' + 
-                Math.round(mousePos.y);
-        var sqMes = 'SQUARE CLICKED: ' + mousePos.x + ', ' + 
-                Math.round(mousePos.y);
-        
-        var xCalc = (canvasWidth + xSize + xPosition) - canvasWidth;
-        var yCalc = (canvasHeight + ySize + yPosition) - canvasHeight;
-
-        //If Mouse Clicked on Square, new message!
-        if (mousePos.x <= xCalc && mousePos.y <= yCalc
-                && mousePos.x >= (xPosition) && mousePos.y >= (yPosition)) {
-                document.getElementById("divShow").innerHTML = sqMes;
-
-        } else { 
-            //writeMessage(canvas, message);
-            //Print the Mouse Co-ords to divShow.
-            document.getElementById("divShow").innerHTML = message;
-        }
-    }, false);
-}
-
-/*
- * Adds a mousedown event listener that specifically deals with
- * mouse co-ordinates on the canvas.
- * 
- * @param {int} xPosition
- * @param {int} yPosition
- * @param {int} xSize
- * @param {int} ySize
- * @returns {undefined}
- */
-function mouseDownCoords(xPosition, yPosition, xSize, ySize) {
-    var canvas = document.getElementById('canvas_1');
-    
-    //The mouseclick event listener.
-    canvas.addEventListener('mousedown', function(evt) {
-        //drawToCanvas(); //refreshes the date/time display
-
-        /*
-         * Grabs mouse position co-ords and creates a 
-         * string out of them to use later.
-         */
-        var mousePos = getMousePos(canvas, evt);
-        var message = 'Mouse position: ' + mousePos.x + ', ' + 
-                Math.round(mousePos.y);
-        var sqMes = 'SQUARE CLICKED: ' + mousePos.x + ', ' + 
-                Math.round(mousePos.y);
-        
-        var xCalc = (canvasWidth + xSize + xPosition) - canvasWidth;
-        var yCalc = (canvasHeight + ySize + yPosition) - canvasHeight;
-
-        //If Mouse Clicked on the Black Square, new message!
-        if (mousePos.x <= xCalc && mousePos.y <= yCalc
-                && mousePos.x >= (xPosition) && mousePos.y >= (yPosition)) {
-                document.getElementById("divShow").innerHTML = sqMes;
-                //emulateVibrate();
-
-        } else { 
-            //writeMessage(canvas, message);
-            //Print the Mouse Co-ords to divShow.
-            document.getElementById("divShow").innerHTML = message;
         }
     }, false);
 }
@@ -255,6 +147,12 @@ function mouseDown(xPosition, yPosition, xSize, ySize, actionTaken) {
     //The mouseclick event listener.
     canvas.addEventListener('mousedown', function(evt) {
         
+        var mousePos = getMousePos(canvas, evt);
+        message = 'Mouse position: ' + mousePos.x + ', ' + 
+                Math.round(mousePos.y);
+        sqMes = 'SQUARE CLICKED: ' + mousePos.x + ', ' + 
+                Math.round(mousePos.y);
+        
         var xCalc = (canvasWidth + xSize + xPosition) - canvasWidth;
         var yCalc = (canvasHeight + ySize + yPosition) - canvasHeight;
 
@@ -270,6 +168,10 @@ function mouseDown(xPosition, yPosition, xSize, ySize, actionTaken) {
 function listeners(xPos, yPos, xSize, ySize, actionToTake) {
     mouseOver(xPos, yPos, xSize, ySize, actionToTake);
     mouseDown(xPos, yPos, xSize, ySize, actionToTake);
+}
+
+function printPosition() {
+    document.getElementById("divShow").innerHTML = sqMes;
 }
 
 //Creates a black rectangle on the canvas with a mouseDown listener
@@ -291,6 +193,40 @@ function dateStringForJSON(toConvert) {
     //At the moment, nothing needs to be converted
     //but later things might.
     return toConvert;
+}
+
+//===============================================================/
+/*
+ * 
+ * Old functions:
+ */
+
+
+//Draws to the canvas - currently a red rectangle and
+// both the current date and time as retrieved by
+// the functions above.
+function drawToCanvas() {
+    var stringTime = createTime();
+    var stringDate = currentDate();
+
+    //Create the Canvas stuff
+    var c = document.getElementById("canvas_1");
+    var ctx = c.getContext("2d");
+    canvasWidth = c.width;
+    canvasHeight = c.height;
+
+    //Changes colour of the Canvas Element
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    //Writes Time as a String to the Canvas
+    ctx.fillStyle = "#000000";
+    ctx.font = "48px Sans Serif";
+    ctx.fillText(stringTime, 10, (canvasHeight/10)+20);
+    ctx.fillText(stringDate, 10, (canvasHeight-20));
+
+    //creates the black rectangle in the middle
+    drawClickRect();
 }
 
 /*
